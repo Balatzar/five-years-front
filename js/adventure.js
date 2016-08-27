e116b6955d67c4dd98856cd3430132b0d98cc21f
balatzar <bataprod@gmail.com>
2016-08-27 22:00:04 +0200

    add localStorage features and refresh

--- js/adventure.js
+++ js/adventure.js
@@ -1,9 +1,7 @@
 /* eslint-env jquery */
 
 (function adventure() {
-  var group = JSON.parse(localStorage.getItem('group'));
-  localStorage.setItem('objectives', JSON.stringify({}));
-  $('.js-titlePage').text(group.name);
+  init();
 
   $('.js-createObjetive').click(function createObjetive() {
     var text = $('[type="text"]').val();
@@ -12,6 +10,12 @@
     }
     var objectives = JSON.parse(localStorage.getItem('objectives'));
     objectives[text] = text;
+    insertObjectiveInDOM(text, objectives);
+    $('[type="text"]').val('');
+    $('.js-finish').removeClass('hidden');
+  });
+
+  function insertObjectiveInDOM(text, objectives) {
     localStorage.setItem('objectives', JSON.stringify(objectives))
     $('.js-objectiveList').append($(makeObjective(text)).children().click(function(event) {
       $(event.currentTarget).parent().remove();
@@ -19,12 +23,10 @@
       delete objectivesInEvent[$(this).parent().get(0).firstChild.nodeValue];
       localStorage.setItem('objectives', JSON.stringify(objectivesInEvent));
       if (!$('.js-objective').length) {
-        $('.js-finish').addClass('hidden')
+        $('.js-finish').addClass('hidden');
       }
     }).parent());
-    $('[type="text"]').val('');
-    $('.js-finish').removeClass('hidden')
-  });
+  }
 
   function makeObjective(str) {
     return `<p class="js-objective">${str}<button type="button" class="button-delete"><span class="glyphicon glyphicon-remove"></span></button></p>`;
@@ -44,5 +46,20 @@
     } else {
       $(selector).css('border-color', '#d00000');
     }
+  }
+
+  function init() {
+    var group = JSON.parse(localStorage.getItem('group'));
+    var objectives = localStorage.getItem('objectives');
+    if (!objectives || objectives === '{}') {
+      localStorage.setItem('objectives', JSON.stringify({}));
+    } else {
+      objectives = JSON.parse(objectives);
+      Object.keys(objectives).forEach(function(text) {
+        insertObjectiveInDOM(text, objectives);
+      });
+      $('.js-finish').removeClass('hidden');
+    }
+    $('.js-titlePage').text(group.name);
   }
 }());
