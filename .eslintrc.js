d964f5b0dd10bb3c9f083026fe390a37837864ea
balatzar <bataprod@gmail.com>
2016-08-28 10:00:13 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,29 @@
 module.exports = {
-    "extends": "airbnb",
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
     "rules": {
-      "consistent-return": 0,
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "comma-dangle": [
+            "error",
+            "always"
+        ],
+        "no-console": 1
     }
 };
